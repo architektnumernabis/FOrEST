536eca0a57511f0726d5d83032bedd25de769f6f
Maciej Pawlica <maciejpawlica@MacBook-Air-Maciej.local>
2024-06-08 20:45:21 +0200

    added header and page structure

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+const slider = document.querySelector('.header__slider')
+
+let sectionIndex = 0
+
+function reset() {
+    for (let i = 0; i < slider.children.length; i++) {
+        slider.children[i].style.opacity = 0
+        slider.children[i].style.zIndex = 0
+    }
+}
+
+function startShow() {
+    setInterval(function () {
+        reset()
+        sectionIndex = sectionIndex < 1 ? sectionIndex + 1 : 0
+        slider.children[sectionIndex].style.opacity = 1
+        slider.children[sectionIndex].style.zIndex = 1
+
+    }, 2000)
+}
+
+startShow()
